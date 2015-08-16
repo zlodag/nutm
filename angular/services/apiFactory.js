701862002840fb79daee7682f882b671dfa618b9
zlodag <zlodag@users.noreply.github.com>
2015-08-17 02:02:06 +1200

    cull

--- angular/services/apiFactory.js
+++
@@ -1,9 +0,0 @@
-angular.module("nutmApp")
-.factory("api", function($resource){
-    return {
-        user: $resource('/api/user/:userId'),
-        task: $resource('/api/task/:taskId')
-        // building: $resource('/api/building/:buildingId'),
-        // ward: $resource('/api/ward/:wardId'),
-    };
-});
